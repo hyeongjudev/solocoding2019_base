49a485ab76b16ce41ba2c7eca7b9807053570081
hyeongju-kim <kinghj.kim@gmail.com>
2019-04-09 01:38:14 +0900

    일단 TextField 추가

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,21 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 // This widget is the root of your application.
+List<String> litems = List();
 class MyApp extends StatefulWidget {
+
   @override
   State<StatefulWidget> createState() => new MyAppState();
 }
 
 class MyAppState extends State<MyApp> {
+
+  List<String> todoListItems = [];
+  final TextEditingController eCtrl = new TextEditingController();
   @override
   Widget build(BuildContext context) {
     // set material design app
+
     return MaterialApp(
       title: 'solocoding2019', // application name
       theme: ThemeData(
@@ -21,10 +27,23 @@ class MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('Flutter Demo'), // app bar title
         ),
-        body: Center(
-          child: Text('Hello, world'), // center text
-        ),
+        body: new Column(
+          children: <Widget>[
+        new TextField(
+        controller: eCtrl,
+          onSubmitted: (text) {
+            litems.add(text);
+            eCtrl.clear();
+            setState(() {});
+          },
+        )
+        ]
+       )
       ),
     );
+  }
+
+  Widget buildBody(BuildContext ctxt, int index) {
+    return Text(litems[index]);
   }
 }
