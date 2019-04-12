f332d550d73df95a307efacea82daaac5bd06e0d
hyeongju-kim <kinghj.kim@gmail.com>
2019-04-13 02:21:50 +0900

    List 디자인 변경

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,7 @@ class  TodoDisplayList extends State<MyApp>{
           children: <Widget>[
             TextField(
               controller: eCtrl,
+              style: TextStyle(fontSize: 18.0),
               decoration: InputDecoration(hintText: "Input Todo", contentPadding : const EdgeInsets.all(20.0),hintStyle: TextStyle(color: Color.fromARGB(255, 187, 189, 191), fontSize: 18),),
               textInputAction: TextInputAction.done,
               onSubmitted: (text) {
@@ -36,12 +37,16 @@ class  TodoDisplayList extends State<MyApp>{
               },
             ),
             Expanded(
-                child: ListView.builder
+                child: ListView.separated
                   (
+                    separatorBuilder: (BuildContext ctxt, int Index) => Divider(
+                      color: Colors.indigo,
+                    ),
                     itemCount: _todoItems.length,
-                    itemBuilder: (BuildContext ctxt, int Index) {
-                      return Text(_todoItems[Index]);
-                    }
+                    itemBuilder: (BuildContext ctxt, int Index) => Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: Text(_todoItems[Index], style: TextStyle(fontSize: 23.0, color: Color.fromARGB(255, 255, 54, 0))),
+                    )
                 )
           )
           ],
