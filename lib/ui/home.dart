92516ba0827ab9c1a421ed80ea98caa2b2c33385
hyeongju-kim <kinghj.kim@gmail.com>
2019-04-16 00:50:59 +0900

    Todo 항목에 따른 Main List 처리

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -14,36 +14,35 @@ class _HomeListState extends State<Home> {
     return Scaffold(
         floatingActionButton: FloatingActionButton(
             child: Icon(Icons.add), onPressed: _showTodoAdd),
-            appBar: AppBar(
-            title: Text("Todo App"),
+        appBar: AppBar(
+          title: Text("Todo App"),
         ),
         body: Column(
           children: <Widget>[
-          /*  Text("할 일 리스트",
-                style: TextStyle(color: Colors.black87, fontSize: 42.0),
-                textAlign: TextAlign.left),*/
-            Text("할 일 없음",
-                style: TextStyle(color: Colors.black87, fontSize: 42.0),
-                textAlign: TextAlign.left),
-            Expanded(
-                child: ListView.separated(
-                    separatorBuilder: (BuildContext ctxt, int Index) => Divider(
-                          color: Colors.indigo,
-                        ),
-                    itemCount: todoItems.length,
-                    itemBuilder: (BuildContext ctxt, int Index) => Padding(
-                          padding: EdgeInsets.all(10.0),
-                          child: Text(todoItems[Index],
-                              style: TextStyle(
-                                  fontSize: 23.0,
-                                  color: Color.fromARGB(255, 255, 54, 0))),
-                        )))
+            todoItems.isEmpty
+                ? ListTile(
+                    title: Text("할 일 없음",
+                        style: TextStyle(color: Colors.black87, fontSize: 42.0),
+                        textAlign: TextAlign.left))
+                : Expanded(
+                    child: ListView.separated(
+                        separatorBuilder: (context, int) => Divider(
+                              color: Colors.indigo,
+                            ),
+                        itemCount: todoItems.length,
+                        itemBuilder: (context, int) => Padding(
+                              padding: EdgeInsets.all(10.0),
+                              child: Text(todoItems[int],
+                                  style: TextStyle(
+                                      fontSize: 23.0,
+                                      color: Color.fromARGB(255, 255, 54, 0))),
+                            )))
           ],
         ));
   }
 
   _showTodoAdd() {
-    Navigator.push(
-        context, MaterialPageRoute(builder: (context) => ToDoAddPage(todoItems)));
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => ToDoAddPage(todoItems)));
   }
 }
--- lib/ui/todoadd.dart
+++ lib/ui/todoadd.dart
@@ -26,21 +26,30 @@ class _ToDoAddState extends State<ToDoAddPage> {
           Text("할 일 추가",
               style: TextStyle(color: Colors.black87, fontSize: 42.0),
               textAlign: TextAlign.left),
-          TextField(
-            controller: eCtrl,
-            style: TextStyle(fontSize: 18.0),
-            decoration: InputDecoration(
-              hintText: "Input Todo",
-              contentPadding: const EdgeInsets.all(20.0),
-              hintStyle: TextStyle(
-                  color: Color.fromARGB(255, 187, 189, 191), fontSize: 18),
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: TextField(
+              controller: eCtrl,
+              style: TextStyle(fontSize: 18.0),
+              decoration: InputDecoration(
+                labelText: "Todo Title",
+                labelStyle: TextStyle(fontSize: 18, color: Colors.blue),
+                hintText: "Input Todo",
+                contentPadding: const EdgeInsets.all(20.0),
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.green)),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black)),
+                hintStyle: TextStyle(
+                    color: Color.fromARGB(255, 187, 189, 191), fontSize: 18),
+              ),
+              textInputAction: TextInputAction.done,
+              onSubmitted: (text) {
+                todoItems.add(text);
+                eCtrl.clear();
+                Navigator.pop(context);
+              },
             ),
-            textInputAction: TextInputAction.done,
-            onSubmitted: (text) {
-              todoItems.add(text);
-              eCtrl.clear();
-              Navigator.pop(context);
-            },
           )
         ]));
   }
