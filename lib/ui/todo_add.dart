7500f641095a1d497689c9cd5df08b5d6dacf2e2
hyeongju-kim <kinghj.kim@gmail.com>
2019-04-17 00:50:35 +0900

    할 일 입력을 버튼으로 처리 미치 예외 처리 추가

--- lib/ui/todo_add.dart
+++ lib/ui/todo_add.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toast/toast.dart';
 
 class ToDoAddPage extends StatefulWidget {
   final List<String> todoList;
@@ -21,38 +22,47 @@ class _ToDoAddState extends State<ToDoAddPage> {
         appBar: AppBar(
           title: Text("Add ToDo"),
         ),
-        body: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text("할 일 추가",
-                  style: TextStyle(color: Colors.black87, fontSize: 42.0),
-                  textAlign: TextAlign.left),
-              Padding(
+        body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: <
+            Widget>[
+          Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text("할 일 추가",
+                    style: TextStyle(color: Colors.black87, fontSize: 42.0),
+                    textAlign: TextAlign.left),
+                IconButton(
+                  icon: Icon(
+                    Icons.add,
+                    color: Colors.blue,
+                  ),
+                  onPressed: () {
+                    _todoAdd();
+                  },
                   padding: const EdgeInsets.all(5.0),
-                  child: TextField(
-                    controller: eCtrl,
-                    style: TextStyle(fontSize: 18.0),
-                    decoration: InputDecoration(
-                      labelText: "Todo Title",
-                      labelStyle: TextStyle(fontSize: 18, color: Colors.blue),
-                      hintText: "Input Todo",
-                      contentPadding: const EdgeInsets.all(20.0),
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.green)),
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black)),
-                      hintStyle: TextStyle(
-                          color: Color.fromARGB(255, 187, 189, 191),
-                          fontSize: 18),
-                    ),
-                    textInputAction: TextInputAction.done,
-                    onSubmitted: (text) {
-                      todoItems.add(text);
-                      eCtrl.clear();
-                      Navigator.pop(context);
-                    },
-                  )),
-              Row(children: <Widget>[
+                ),
+              ]),
+          Padding(
+              padding: const EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 20.0),
+              child: TextField(
+                controller: eCtrl,
+                style: TextStyle(fontSize: 18.0),
+                decoration: InputDecoration(
+                  labelText: "Todo Title",
+                  labelStyle: TextStyle(fontSize: 18, color: Colors.blue),
+                  hintText: "Input Todo",
+                  contentPadding: const EdgeInsets.all(20.0),
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green)),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black)),
+                  hintStyle: TextStyle(
+                      color: Color.fromARGB(255, 187, 189, 191), fontSize: 18),
+                ),
+                textInputAction: TextInputAction.done
+              )),
+          Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
                 RaisedButton(
                   onPressed: () {},
                   textColor: Colors.white,
@@ -82,6 +92,16 @@ class _ToDoAddState extends State<ToDoAddPage> {
                   ),
                 ),
               ])
-            ]));
+        ]));
+  }
+
+  _todoAdd() {
+    if(eCtrl.text.trim() == "") {
+      Toast.show("할 일을 입력 하세요", context, duration: Toast.LENGTH_LONG, gravity:  Toast.BOTTOM);
+    } else {
+      todoItems.add(eCtrl.text);
+      eCtrl.clear();
+      Navigator.pop(context);
+    }
   }
 }
